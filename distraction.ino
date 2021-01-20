3c259219ab4abec3124e891fab7463d6d0321faf
fzdxgchvmmnbv <76603406+fzdxgchvmmnbv@users.noreply.github.com>
2021-01-20 13:53:01 +0200

    Add files via upload

---
+++ distraction.ino
@@ -0,0 +1,36 @@
+///you just got distracted
+///warning : this meme has been dead for months
+///this meme is deader then uncle ben
+///if you post it on social media please give credit
+///I stole the track from https://musescore.com/user/19710181/scores/6306481
+
+///buzzer on pin A7
+
+
+
+
+void setup() {
+delay(1500);}  //small delay so I can turn on the camera
+
+
+
+
+void loop() {
+tone(A7, 277, 281);     //yes I know, for loops exist, but the melody is so simple it didn't deserve one
+delay(562);
+tone(A7, 370, 281);
+//[clap and footstomp]
+delay(468);
+tone(A7, 277, 281);
+delay(356);                        //the melody isn't perfect, but this was the closest I could get
+tone(A7, 220, 187);                       //plus this meme is like super-dead no one cares
+delay(197);
+tone(A7, 233, 187);
+delay(197);
+tone(A7, 247, 281);
+delay(281);
+}
+
+
+
+  
